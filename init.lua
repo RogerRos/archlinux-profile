11a42fafe7a6c1de4cabc43566dc0d5c0d72211a
Roger Ros <128326832+RogerRos@users.noreply.github.com>
2025-10-20 13:04:42 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -1,5 +1,30 @@
+-- Basic settings
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.termguicolors = true
+vim.o.mouse = "a"
+vim.o.clipboard = "unnamedplus"
+vim.o.expandtab = true
+vim.o.shiftwidth = 2
+vim.o.tabstop = 2
+vim.o.smartindent = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.wrap = false
+vim.o.cursorline = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+-- Enable filetype, plugins and syntax
 vim.cmd([[
   filetype plugin indent on
   syntax on
-  set clipboard=unnamedplus
 ]])
+
+-- TokyoNight colorscheme
+require("tokyonight").setup({
+  style = "moon",
+  transparent = false,
+  terminal_colors = true,
+})
+vim.cmd("colorscheme tokyonight")
