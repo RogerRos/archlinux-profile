a85620724f619382feee2f467035a9e87c72a916
Roger Ros <128326832+RogerRos@users.noreply.github.com>
2025-10-20 13:15:18 -0700

    Update init.lua

--- init.lua
+++ init.lua
@@ -27,4 +27,40 @@ require("tokyonight").setup({
   transparent = false,
   terminal_colors = true,
 })
-vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme tokyonight-moon")
+
+-- File explorer (nvim-tree)
+require("nvim-tree").setup({
+  sort_by = "name",
+  view = {
+    width = 30,
+    side = "left",
+  },
+  renderer = {
+    group_empty = true,
+    highlight_git = true,
+    icons = {
+      show = {
+        file = true,
+        folder = true,
+        folder_arrow = true,
+        git = true,
+      },
+    },
+  },
+  filters = {
+    dotfiles = false,
+  },
+})
+
+-- Keymap to toggle tree
+vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- Auto open tree if starting in a directory
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    if vim.fn.isdirectory(vim.fn.expand("%")) == 1 then
+      require("nvim-tree.api").tree.open()
+    end
+  end,
+})
